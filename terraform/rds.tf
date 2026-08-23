2ccfbbf2aaade3e26ca441948d921d44e2e9e41b
Durga Bhavani <durgabhavaniv77@gmail.com>
2026-08-23 13:52:46 +0530

    Fix RDS engine to aurora-postgresql and set EKS instance type to t3.micro for free tier

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -58,8 +58,8 @@ resource "aws_security_group" "users_db" {
 
 resource "aws_rds_cluster" "users" {
   cluster_identifier     = "${var.project_name}-${var.environment}-users-db"
-  engine                 = "aurora-mysql"
-  engine_mode            = "provisioned"
+  engine                 = "aurora-postgresql"
+  engine_mode            = "15.4"
   # No engine_version pinned on purpose: Aurora MySQL minor versions get
   # retired periodically, and a hardcoded one (e.g. 3.07.1) will eventually
   # 400 with "Cannot find version ... for aurora-mysql". Omitting it lets
